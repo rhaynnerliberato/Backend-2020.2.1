c0ed40e4de35c8406b4191734132e5e8ef720229
rhaynner <rhay_liberato@hotmail.com>
2020-09-14 14:32:01 -0300

    Adicionei o EndPoint HelloWorld.

---
+++ Backend-2020.2.1/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_2020._2._1.Controllers
+{
+    [ApiController]
+    [Route("HelloWorld")]
+    public class HelloWorldController : ControllerBase
+    {
+        public HelloWorldController(){}
+        [HttpGet]
+        public string Get()
+        {
+            //Qualquer coisa aqui dentro
+            return "HelloWorld";
+        }
+    }
+}
